2e17b394faa089431314c9056c10234cf9d92273
Icydeath <6233937+Icydeath@users.noreply.github.com>
2020-05-29 22:13:40 -0400

    added mhaura ambuscade messages

--- InstanceNotifier.lua
+++ InstanceNotifier.lua
@@ -1,44 +1,49 @@
 _addon.name = 'InstanceNotifier'
 _addon.author = 'Icy'
 _addon.commands = {'InstanceNotifier'}
-_addon.version = '0.0.1'
+_addon.version = '0.0.2'
+-- added Mhaura's Ambuscade messages.
 
 require('tables')
 require('strings')
 require('logger')
 local enabled = false
 
+valid_zones = T{
+	248, -- selbina
+	249, -- mhaura
+}
+
 msgs = T{
 	'Your registration number for entering this nightmare is', --148
 	'Nightmare number',
-	'Entering '
+	'Entering ',
+	' of the tome flares up',
+	' while the battle is being prepared',
 }
 
 windower.register_event('addon command', function(...)
-    local command = {...}
+    local commands = {...}
 	
 end)
 
 windower.register_event("incoming text", function(original,modified,original_mode,modified_mode, blocked)
-	if enabled then
-		if original_mode == 151 or original_mode == 161 or original_mode == 190 or original_mode == 148 then
-			--windower.add_to_chat(8, '  ID = '..original_mode)
-			for i,msg in pairs(msgs) do
-				if windower.wc_match(original, "*"..msg.."*") then
-					--if msg:contains('Entering') then
-						--windower.send_command('input /p '.. original .. ' <call14>')
-					--else
-						windower.send_command('input /p '.. original)
-					--end
-					break
-				end
+  if enabled then
+	if original_mode == 151 or original_mode == 161 or original_mode == 190 or original_mode == 148 then
+		--windower.add_to_chat(8, '  ID = '..original_mode)
+		for i,msg in pairs(msgs) do
+			if windower.wc_match(original, "*"..msg.."*") then
+				windower.send_command('input /p '.. original)
+				break
 			end
 		end
-    end
+	end
+  end
 end)
 
 windower.register_event('zone change','login','load', function()
-    if windower.ffxi.get_info().zone == 248 then
+	local zone = windower.ffxi.get_info().zone
+	if valid_zones[zone] ~= nil then
 		local player = windower.ffxi.get_player()
 		local party = windower.ffxi.get_party()
 		if player.id == party.party1_leader then
@@ -50,6 +55,6 @@ windower.register_event('zone change','login','load', function()
 		end
 	else
 		enabled = false
-		log('Not in Selbina. enabled =', tostring(enabled))
+		log('Not in a valid zone. enabled =', tostring(enabled))
 	end
 end)
